2e49c9a18253d4b69c0c261526ef200c9968c20a
Paul Wassermann <paul9499@gmail.com>
2025-11-11 17:12:20 +0100

    feat: print macro can optionally print a newline

--- src/commons/print.s
+++ src/commons/print.s
@@ -6,16 +6,20 @@
 //      - r2 to accumulate the number of characters in the string
 //      - r3 to store the current character
 
-.macro  print   str:req
+.data
+newline_str:        .string         "\n"
+
+.text
+.macro  print   str:req, newline=0
 
         ldr     r1, =\str
         mov     r2, #0
 
 1:
         ldrb    r3, [r1, r2]
-        add     r2, #1
         cmp     r3, #0
         beq     2f
+        add     r2, #1
         b       1b
 
 2:
@@ -24,4 +28,12 @@
         // r1 and r2 are already set
         svc     0
 
+        .if \newline
+                mov     r7, #4
+                mov     r0, #1
+                ldr     r1, =newline_str
+                mov     r2, #1
+                svc     0
+        .endif
 .endm
+
